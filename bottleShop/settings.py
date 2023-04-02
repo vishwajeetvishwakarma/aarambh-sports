667b93010aa55a7bb5fe6fa045d9024870ce51d7
vishwajeetvishwakarma <vishwajeetv2003@gmail.com>
2023-04-02 10:38:08 +0530

    before razorpay integrations

--- bottleShop/settings.py
+++ bottleShop/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,5 +145,3 @@ JET_SIDE_MENU_COMPACT = True
 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-django_heroku.settings(locals())
